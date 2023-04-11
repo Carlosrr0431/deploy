1d83eeb3b15483de7cf04cd0ce28f0f0e581e43c
Carlos RR <carlos.facundo.rr@gmail.com>
2023-04-11 18:22:35 -0300

    modificacion en el servidor

--- server/index.js
+++ server/index.js
@@ -4,5 +4,11 @@ import {PORT} from './config.js';
 import app from './app.js';
 
 conectDb();
+
+app.use("/", (req, res) => {
+  res.send('Bienvenido al servidor de home page');
+});
 app.listen(PORT);
+
+
  
